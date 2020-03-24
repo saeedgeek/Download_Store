06e6c7fb29873b252909008c4272c275859963cd
saeed zare <szare295@gmail.com>
2020-03-25 03:02:02 +0430

    miner fix

--- fileStore/production/models.py
+++ fileStore/production/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(AbstractUser):
+     accountMoney=models.SmallIntegerField()
+     class Meta:
+        swappable = 'AUTH_USER_MODEL'
+        verbose_name = 'Profile' 
+
+class Admin(models.Model):
+     user=models.OneToOneField(Profile,on_delete=models.CASCADE)
+
+class Customer(models.Model):
+     user=models.OneToOneField(Profile,on_delete=models.CASCADE)
+
