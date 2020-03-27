7aea6a51b061e8cb8d1890369546d86a158eaddf
saeed zare <szare295@gmail.com>
2020-03-28 03:40:11 +0430

    create sotre4Admin permission

--- fileStore/utils/permissions.py
+++ fileStore/utils/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework.permissions import BasePermission
 from user.models import Admin,Customer
-
+from store.models import Store
 
 class AdminPermission(BasePermission):
      def has_permission(self, request, view):
@@ -12,6 +12,25 @@ class AdminPermission(BasePermission):
                     return False     
           except:
                return False
+
+class StoreForThisAdmin(BasePermission):
+     def has_permission(self, request, view):
+          try:
+               store_requested=request.data["store"]
+               admin=Admin.objects.get(user=request.user)
+               stores=Store.objects.filter(admin=admin)
+               list_of_store_name=[]
+               for store in stores:
+                    list_of_store_name.append(store.name)
+               print(list_of_store_name)
+               print(store_requested)
+               if store_requested in list_of_store_name:
+                    return True
+               else:
+                    return False
+          except:
+               return False
+
 
 
 class CustomerPersmission(BasePermission):
